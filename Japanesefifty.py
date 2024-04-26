2d55345ded29db093a43ccfbb297098f72e1732c
Ruer.Sun <lqy9913512@qq.com>
2024-04-26 23:13:40 +0800

    支持片假名

--- Japanesefifty.py
+++ Japanesefifty.py
@@ -2,19 +2,29 @@ from random import choice
 from os import system
 import sys  
 
-def loadData():
-    fiftyTones = {"あ" : "a","か" : "ka","さ" : "sa","た" : "ta","な" : "na","は" : "ha","ま" : "ma","や" : "ya","ら" : "ra","わ" : "wa",
-                  "い" : "i","き" : "ki","し" : "shi","ち" : "chi","に" : "ni","ひ" : "hi","み" : "mi","い" : "i","り" : "ri","い" : "i",
-                  "う" : "u","く" : "ku","す" : "su","つ" : "tsu","ぬ" : "nu","ふ" : "fu","む" : "mu","ゆ" : "yu","る" : "ru","う" : "u",
-                  "え" : "e","け" : "ke","せ" : "se","て" : "te","ね" : "ne","へ" : "he","め" : "me","え" : "e","れ" : "re","え" : "e",
-                  "お" : "o","こ" : "ko","そ" : "so","と" : "to","の" : "no","ほ" : "ho","も" : "mo","よ" : "yo","ろ" : "ro","を" : "(w)o"}
+def loadHiraganaData():
+    fiftyTones = {"あ": "a", "か": "ka", "さ": "sa", "た": "ta", "な": "na", "は": "ha", "ま": "ma", "や": "ya", "ら": "ra", "わ": "wa",
+                  "い": "i", "き": "ki", "し": "shi", "ち": "chi", "に": "ni", "ひ": "hi", "み": "mi", "り": "ri",
+                  "う": "u", "く": "ku", "す": "su", "つ": "tsu", "ぬ": "nu", "ふ": "fu", "む": "mu", "ゆ": "yu", "る": "ru",
+                  "え": "e", "け": "ke", "せ": "se", "て": "te", "ね": "ne", "へ": "he", "め": "me", "れ": "re",
+                  "お": "o", "こ": "ko", "そ": "so", "と": "to", "の": "no", "ほ": "ho", "も": "mo", "よ": "yo", "ろ": "ro", "を": "(w)o"}
     return fiftyTones
+
+
+def loadKatakanaData():
+    fiftyTones = {"ア": "a", "カ": "ka", "サ": "sa", "タ": "ta", "ナ": "na", "ハ": "ha", "マ": "ma", "ヤ": "ya", "ラ": "ra", "ワ": "wa",
+                  "イ": "i", "キ": "ki", "シ": "shi", "チ": "chi", "ニ": "ni", "ヒ": "hi", "ミ": "mi", "リ": "ri",
+                  "ウ": "u", "ク": "ku", "ス": "su", "ツ": "tsu", "ヌ": "nu", "フ": "fu", "ム": "mu", "ユ": "yu", "ル": "ru",
+                  "エ": "e", "ケ": "ke", "セ": "se", "テ": "te", "ネ": "ne", "ヘ": "he", "メ": "me", "レ": "re",
+                  "オ": "o", "コ": "ko", "ソ": "so", "ト": "to", "ノ": "no", "ホ": "ho", "モ": "mo", "ヨ": "yo", "ロ": "ro", "ヲ": "(w)o"}
+    return fiftyTones
+
 
 def loadVoicedSoundData():
     voicedSound = {"が": "ga", "ぎ": "gi", "ぐ": "gu", "げ": "ge", "ご": "go", 
                    "ざ": "za", "じ": "zi", "ず": "zu", "ぜ": "ze", "ぞ": "zo", 
                    "だ": "da", "ぢ": "zi", "づ": "zu", "で": "de", "ど": "do", 
-                   "ば": "ba", "び": "bi", "ぶ": "bu", "べ": "be", "ぼ": "bo",  
+                   "ば": "ba", "び": "bi", "ぶ": "bu", "べ": "be", "ぼ": "bo", 
                    "ぱ": "pa", "ぴ": "pi", "ぷ": "pu", "ぺ": "pe", "ぽ": "po"}
     return voicedSound
 
@@ -28,9 +38,9 @@ def loadProtonationData():
     return protonationData
 
 def loadIntonationData():
-    intonationData={"きゃ": "kya", "きゅ": "kyu", "きょ": "kyo", "しゃ": "sya", "しゅ": "syu", "しょ": "syo", "ちゃ": "tya", "ちゅ": "tyu", "ちょ": "tyo",
+    intonationData={"きゃ": "kya", "きゅ": "kyu", "きょ": "kyo", "しゃ": "sha", "しゅ": "shu", "しょ": "sho", "ちゃ": "cha", "ちゅ": "chu", "ちょ": "cho",
                      "にゃ": "nya", "にゅ": "nyu", "にょ": "nyo", "ひゃ": "hya", "ひゅ": "hyu", "ひょ": "hyo", "みゃ": "mya", "みゅ": "myu", "みょ": "myo",
-                     "りゃ": "rya", "りゅ": "ryu", "りょ": "ryo", "ぎゃ": "gya", "ぎゅ": "gyu", "ぎょ": "gyo", "じゃ": "zya", "じゅ": "zyu", "じょ": "zyo",
+                     "りゃ": "rya", "りゅ": "ryu", "りょ": "ryo", "ぎゃ": "gya", "ぎゅ": "gyu", "ぎょ": "gyo", "じゃ": "ja", "じゅ": "ju", "じょ": "jo",
                      "ぢゃ": "dya", "ぢゅ": "dyu", "ぢょ": "dyo", "びゃ": "bya", "びゅ": "byu", "びょ": "byo", "ぴゃ": "pya", "ぴゅ": "pyu", "ぴょ": "pyo"}
     return intonationData
 
@@ -39,6 +49,10 @@ def randomLoad(data):
     randomdatakey = choice(list(data.keys()))
     randomdatavalue = data[randomdatakey]
     return randomdatakey, randomdatavalue
+
+
+def keyValDel(data, randomdatakey):
+    del data[randomdatakey]
 
 
 def trainAndcheck(randomdatakey, randomdatavalue):
@@ -57,7 +71,6 @@ def trainAndcheck(randomdatakey, randomdatavalue):
         print("the word correct pronunciation is : ", randomdatavalue)
         print("\n\n")
         return False
-    
 
 
 def showData(selected, data):
@@ -69,20 +82,25 @@ def showData(selected, data):
         if count==10:
             count=0
             print("\n")
-    if selected=="1":
+    if selected=='1':
         print("\n\nNotes:\n\
           \tthese words pronunciation is special:\n\n\
           'し'读:'xi', 'ち'读:'qi', 'つ'读:'ci', 'ふ'读:'fu', 'r'读:'l'\n\
           \tn 作为辅音")
-    if selected=="2":
+    if selected=='2':
+        print("\n\nNotes:\n\
+          \tthese words pronunciation is special:\n\n\
+          'シ'读:'xi', 'チ'读:'qi', 'ツ'读:'ci', 'フ'读:'fu', 'r'读:'l'\n\
+          \tn 作为辅音")
+    if selected=='3':
         print("\n\nNotes:\n\
           \tthese words pronunciation is special:\n\n\
           'じ'读:'ji', 'ぢ'读:'ji', 'ず'读:'zi', 'づ'读:'zi'\n")
-    if selected=="3":
+    if selected=='4':
         print("\n\nNotes:\n\
           \t例如: あ 行指 か さ た な は ま や ら わ \n\
           \tあ行 + あ 发长音")
-    if selected=="5":
+    if selected=='6':
         print('\n\nNotes:\n\
           \t"しゃ": "sya" 读:xya, "しゅ": "syu" 读:xyu, "しょ": "syo" 读:xyo, "ちゃ": "tya" 读:qya, "ちゅ": "tyu" 读:qyu, "ちょ": "tyo" 读:qyo,\n\
           \t"じゃ": "zya" 读:jya, "じゅ": "zyu" 读:jyu, "じょ": "zyo" 读:jyo, "ぢゃ": "dya" 读:jya, "ぢゅ": "dyu" 读:jyu, "ぢょ": "dyo" 读:jyo')
@@ -91,27 +109,31 @@ def showData(selected, data):
         input("\npress <enter> to clear these tones")
     system("cls")
 
-def runTrain(selected):
+def loadData(selected):
+    data = []
+    _data = []
+
     if selected == '1':
-        data = loadData()
-    elif selected == "2":
+        data = loadHiraganaData()
+    elif selected == '2':
+        data = loadKatakanaData()
+    elif selected == '3':
         data = loadVoicedSoundData()
-    elif selected == "3":
+    elif selected == '4':
         data = loadLongVoweldata()
-        _data = loadData()
-        showData(selected, _data)
-        showData(selected, data)
-        return True
-    elif selected == "4":
+        _data = loadHiraganaData()
+    elif selected == '5':
         data = loadProtonationData()
-        showData(selected, data)
-        return True
-    elif selected == "5":
+    elif selected == '6':
         data = loadIntonationData()
-    else:
-        print("no selected")
-        return False
-    showData(selected, data)
+    return data, _data
+
+def runTrain(selected):
+    data, _data = loadData(selected)
+    if len(data) > 0:
+        showData(selected, data)
+    if len(_data) > 0:
+        showData(selected, _data)
     while True:
         if sys.stdin.isatty():
             next = input("do you want to start the train ?(y/n)[default is y,press <enter>]: ")
@@ -125,15 +147,19 @@ def runTrain(selected):
                 print("无法在非终端环境中读取输入")
             if not times:
                 times=10
+            data, _data = loadData(selected)
             right=0
             error=0
             for time in range(int(times)):
                 randomdatakey, randomdatavalue = randomLoad(data)
-                print(f"Progress Bar: {time+1}/{times}\n")
+                print(f"Progress Bar: {time+1}/{times} ", "(", len(data), ")\n")
                 if trainAndcheck(randomdatakey, randomdatavalue):
+                    keyValDel(data, randomdatakey)
                     right=right+1
                 else:
                     error=error+1
+                if len(data) == 0:
+                    data, _data = loadData(selected)
             print("right times: ", right)
             print("error times: ", error)
             print("accuracy: ",right/int(times))
@@ -143,13 +169,14 @@ def runTrain(selected):
 
 
 def menu():
-    print("====================     MENU    ====================")
-    print("\n========== 1. 日语五十音看字识音训练\t==========")
-    print("\n========== 2. 日语浊音看字识音训练\t==========")
-    print("\n========== 3. 日语延长音组合\t\t==========")
-    print("\n========== 4. 日语促音(停顿音)组合\t==========")
-    print("\n========== 5. 日语拗音看字识音训练\t==========")
-    print("\n========== 0. 退出\t\t\t==========")
+    print("=======================     MENU    =======================")
+    print("\n========== 1. 日语五十音看字识音训练(平假名)\t==========")
+    print("\n========== 2. 日语五十音看字识音训练(片假名)\t==========")
+    print("\n========== 3. 日语浊音看字识音训练\t\t==========")
+    print("\n========== 4. 日语延长音组合\t\t\t==========")
+    print("\n========== 5. 日语促音(停顿音)组合\t\t==========")
+    print("\n========== 6. 日语拗音看字识音训练\t\t==========")
+    print("\n========== 0. 退出\t\t\t\t==========")
     print("\n\t\t\t\t\tPowered By Joycat")
 
 
