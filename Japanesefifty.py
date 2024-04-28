265000d2f04d24d168f8dd51d04f4f5758cd1100
luozy <luozy@yealink.com>
2024-04-28 17:52:13 +0800

    支持平假片假混合测试

--- Japanesefifty.py
+++ Japanesefifty.py
@@ -3,7 +3,7 @@ from os import system
 import sys  
 
 def loadHiraganaData():
-    fiftyTones = {"あ": "a", "か": "ka", "さ": "sa", "た": "ta", "な": "na", "は": "ha", "ま": "ma", "や": "ya", "ら": "ra", "わ": "wa",
+    fiftyTones = {"あ": "a", "か": "ka", "さ": "sa", "た": "ta", "な": "na", "は": "ha", "ま": "ma", "や": "ya", "ら": "ra", "わ": "wa", "ん": "n",
                   "い": "i", "き": "ki", "し": "shi", "ち": "chi", "に": "ni", "ひ": "hi", "み": "mi", "り": "ri",
                   "う": "u", "く": "ku", "す": "su", "つ": "tsu", "ぬ": "nu", "ふ": "fu", "む": "mu", "ゆ": "yu", "る": "ru",
                   "え": "e", "け": "ke", "せ": "se", "て": "te", "ね": "ne", "へ": "he", "め": "me", "れ": "re",
@@ -12,7 +12,21 @@ def loadHiraganaData():
 
 
 def loadKatakanaData():
-    fiftyTones = {"ア": "a", "カ": "ka", "サ": "sa", "タ": "ta", "ナ": "na", "ハ": "ha", "マ": "ma", "ヤ": "ya", "ラ": "ra", "ワ": "wa",
+    fiftyTones = {"ア": "a", "カ": "ka", "サ": "sa", "タ": "ta", "ナ": "na", "ハ": "ha", "マ": "ma", "ヤ": "ya", "ラ": "ra", "ワ": "wa", "ン": "n",
+                  "イ": "i", "キ": "ki", "シ": "shi", "チ": "chi", "ニ": "ni", "ヒ": "hi", "ミ": "mi", "リ": "ri",
+                  "ウ": "u", "ク": "ku", "ス": "su", "ツ": "tsu", "ヌ": "nu", "フ": "fu", "ム": "mu", "ユ": "yu", "ル": "ru",
+                  "エ": "e", "ケ": "ke", "セ": "se", "テ": "te", "ネ": "ne", "ヘ": "he", "メ": "me", "レ": "re",
+                  "オ": "o", "コ": "ko", "ソ": "so", "ト": "to", "ノ": "no", "ホ": "ho", "モ": "mo", "ヨ": "yo", "ロ": "ro", "ヲ": "(w)o"}
+    return fiftyTones
+
+
+def loadHiraganaKatakanaData():
+    fiftyTones = {"あ": "a", "か": "ka", "さ": "sa", "た": "ta", "な": "na", "は": "ha", "ま": "ma", "や": "ya", "ら": "ra", "わ": "wa", "ん": "n",
+                  "い": "i", "き": "ki", "し": "shi", "ち": "chi", "に": "ni", "ひ": "hi", "み": "mi", "り": "ri",
+                  "う": "u", "く": "ku", "す": "su", "つ": "tsu", "ぬ": "nu", "ふ": "fu", "む": "mu", "ゆ": "yu", "る": "ru",
+                  "え": "e", "け": "ke", "せ": "se", "て": "te", "ね": "ne", "へ": "he", "め": "me", "れ": "re",
+                  "お": "o", "こ": "ko", "そ": "so", "と": "to", "の": "no", "ほ": "ho", "も": "mo", "よ": "yo", "ろ": "ro", "を": "(w)o",
+                  "ア": "a", "カ": "ka", "サ": "sa", "タ": "ta", "ナ": "na", "ハ": "ha", "マ": "ma", "ヤ": "ya", "ラ": "ra", "ワ": "wa", "ン": "n",
                   "イ": "i", "キ": "ki", "シ": "shi", "チ": "chi", "ニ": "ni", "ヒ": "hi", "ミ": "mi", "リ": "ri",
                   "ウ": "u", "ク": "ku", "ス": "su", "ツ": "tsu", "ヌ": "nu", "フ": "fu", "ム": "mu", "ユ": "yu", "ル": "ru",
                   "エ": "e", "ケ": "ke", "セ": "se", "テ": "te", "ネ": "ne", "ヘ": "he", "メ": "me", "レ": "re",
@@ -92,15 +106,15 @@ def showData(selected, data):
           \tthese words pronunciation is special:\n\n\
           'シ'读:'xi', 'チ'读:'qi', 'ツ'读:'ci', 'フ'读:'fu', 'r'读:'l'\n\
           \tn 作为辅音")
-    if selected=='3':
+    if selected=='4':
         print("\n\nNotes:\n\
           \tthese words pronunciation is special:\n\n\
           'じ'读:'ji', 'ぢ'读:'ji', 'ず'读:'zi', 'づ'读:'zi'\n")
-    if selected=='4':
+    if selected=='5':
         print("\n\nNotes:\n\
           \t例如: あ 行指 か さ た な は ま や ら わ \n\
           \tあ行 + あ 发长音")
-    if selected=='6':
+    if selected=='7':
         print('\n\nNotes:\n\
           \t"しゃ": "sya" 读:xya, "しゅ": "syu" 读:xyu, "しょ": "syo" 读:xyo, "ちゃ": "tya" 读:qya, "ちゅ": "tyu" 读:qyu, "ちょ": "tyo" 读:qyo,\n\
           \t"じゃ": "zya" 读:jya, "じゅ": "zyu" 读:jyu, "じょ": "zyo" 读:jyo, "ぢゃ": "dya" 读:jya, "ぢゅ": "dyu" 读:jyu, "ぢょ": "dyo" 读:jyo')
@@ -118,13 +132,15 @@ def loadData(selected):
     elif selected == '2':
         data = loadKatakanaData()
     elif selected == '3':
-        data = loadVoicedSoundData()
+        data = loadHiraganaKatakanaData()
     elif selected == '4':
+        data = loadVoicedSoundData()
+    elif selected == '5':
         data = loadLongVoweldata()
         _data = loadHiraganaData()
-    elif selected == '5':
-        data = loadProtonationData()
     elif selected == '6':
+        data = loadProtonationData()
+    elif selected == '7':
         data = loadIntonationData()
     return data, _data
 
@@ -169,15 +185,16 @@ def runTrain(selected):
 
 
 def menu():
-    print("=======================     MENU    =======================")
-    print("\n========== 1. 日语五十音看字识音训练(平假名)\t==========")
-    print("\n========== 2. 日语五十音看字识音训练(片假名)\t==========")
-    print("\n========== 3. 日语浊音看字识音训练\t\t==========")
-    print("\n========== 4. 日语延长音组合\t\t\t==========")
-    print("\n========== 5. 日语促音(停顿音)组合\t\t==========")
-    print("\n========== 6. 日语拗音看字识音训练\t\t==========")
-    print("\n========== 0. 退出\t\t\t\t==========")
-    print("\n\t\t\t\t\tPowered By Joycat")
+    print("==========================     MENU    ==========================")
+    print("\n========== 1. 日语五十音看字识音训练(平假名)\t\t==========")
+    print("\n========== 2. 日语五十音看字识音训练(片假名)\t\t==========")
+    print("\n========== 3. 日语五十音看字识音训练(平假名&片假名)\t==========")
+    print("\n========== 4. 日语浊音看字识音训练\t\t\t==========")
+    print("\n========== 5. 日语延长音组合\t\t\t\t==========")
+    print("\n========== 6. 日语促音(停顿音)组合\t\t\t==========")
+    print("\n========== 7. 日语拗音看字识音训练\t\t\t==========")
+    print("\n========== 0. 退出\t\t\t\t\t==========")
+    print("\n\t\t\t\t\t\tPowered By Ruer")
 
 
 
