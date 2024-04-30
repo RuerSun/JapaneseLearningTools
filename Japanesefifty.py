6d103df2b9379b125f36bf91da3b82176e385864
luozy <luozy@yealink.com>
2024-04-30 08:58:25 +0800

    支持所有内容混合测试

--- Japanesefifty.py
+++ Japanesefifty.py
@@ -51,12 +51,36 @@ def loadProtonationData():
     protonationData={"小写的つ": "例如: あさって (明後日)[键盘键入 a sa t te], 功能是用于发音停顿"}
     return protonationData
 
+
 def loadIntonationData():
     intonationData={"きゃ": "kya", "きゅ": "kyu", "きょ": "kyo", "しゃ": "sha", "しゅ": "shu", "しょ": "sho", "ちゃ": "cha", "ちゅ": "chu", "ちょ": "cho",
                      "にゃ": "nya", "にゅ": "nyu", "にょ": "nyo", "ひゃ": "hya", "ひゅ": "hyu", "ひょ": "hyo", "みゃ": "mya", "みゅ": "myu", "みょ": "myo",
                      "りゃ": "rya", "りゅ": "ryu", "りょ": "ryo", "ぎゃ": "gya", "ぎゅ": "gyu", "ぎょ": "gyo", "じゃ": "ja", "じゅ": "ju", "じょ": "jo",
                      "ぢゃ": "dya", "ぢゅ": "dyu", "ぢょ": "dyo", "びゃ": "bya", "びゅ": "byu", "びょ": "byo", "ぴゃ": "pya", "ぴゅ": "pyu", "ぴょ": "pyo"}
     return intonationData
+
+
+def loadAllData():
+    allData={"あ": "a", "か": "ka", "さ": "sa", "た": "ta", "な": "na", "は": "ha", "ま": "ma", "や": "ya", "ら": "ra", "わ": "wa", "ん": "n",
+                    "い": "i", "き": "ki", "し": "shi", "ち": "chi", "に": "ni", "ひ": "hi", "み": "mi", "り": "ri",
+                    "う": "u", "く": "ku", "す": "su", "つ": "tsu", "ぬ": "nu", "ふ": "fu", "む": "mu", "ゆ": "yu", "る": "ru",
+                    "え": "e", "け": "ke", "せ": "se", "て": "te", "ね": "ne", "へ": "he", "め": "me", "れ": "re",
+                    "お": "o", "こ": "ko", "そ": "so", "と": "to", "の": "no", "ほ": "ho", "も": "mo", "よ": "yo", "ろ": "ro", "を": "(w)o",
+                    "ア": "a", "カ": "ka", "サ": "sa", "タ": "ta", "ナ": "na", "ハ": "ha", "マ": "ma", "ヤ": "ya", "ラ": "ra", "ワ": "wa", "ン": "n",
+                    "イ": "i", "キ": "ki", "シ": "shi", "チ": "chi", "ニ": "ni", "ヒ": "hi", "ミ": "mi", "リ": "ri",
+                    "ウ": "u", "ク": "ku", "ス": "su", "ツ": "tsu", "ヌ": "nu", "フ": "fu", "ム": "mu", "ユ": "yu", "ル": "ru",
+                    "エ": "e", "ケ": "ke", "セ": "se", "テ": "te", "ネ": "ne", "ヘ": "he", "メ": "me", "レ": "re",
+                    "オ": "o", "コ": "ko", "ソ": "so", "ト": "to", "ノ": "no", "ホ": "ho", "モ": "mo", "ヨ": "yo", "ロ": "ro", "ヲ": "(w)o",
+                    "が": "ga", "ぎ": "gi", "ぐ": "gu", "げ": "ge", "ご": "go", 
+                    "ざ": "za", "じ": "zi", "ず": "zu", "ぜ": "ze", "ぞ": "zo", 
+                    "だ": "da", "ぢ": "zi", "づ": "zu", "で": "de", "ど": "do", 
+                    "ば": "ba", "び": "bi", "ぶ": "bu", "べ": "be", "ぼ": "bo", 
+                    "ぱ": "pa", "ぴ": "pi", "ぷ": "pu", "ぺ": "pe", "ぽ": "po",
+                    "きゃ": "kya", "きゅ": "kyu", "きょ": "kyo", "しゃ": "sha", "しゅ": "shu", "しょ": "sho", "ちゃ": "cha", "ちゅ": "chu", "ちょ": "cho",
+                    "にゃ": "nya", "にゅ": "nyu", "にょ": "nyo", "ひゃ": "hya", "ひゅ": "hyu", "ひょ": "hyo", "みゃ": "mya", "みゅ": "myu", "みょ": "myo",
+                    "りゃ": "rya", "りゅ": "ryu", "りょ": "ryo", "ぎゃ": "gya", "ぎゅ": "gyu", "ぎょ": "gyo", "じゃ": "ja", "じゅ": "ju", "じょ": "jo",
+                    "ぢゃ": "dya", "ぢゅ": "dyu", "ぢょ": "dyo", "びゃ": "bya", "びゅ": "byu", "びょ": "byo", "ぴゃ": "pya", "ぴゅ": "pyu", "ぴょ": "pyo"}
+    return allData
 
 
 def randomLoad(data):
@@ -142,6 +166,8 @@ def loadData(selected):
         data = loadProtonationData()
     elif selected == '7':
         data = loadIntonationData()
+    elif selected == '8':
+        data = loadAllData()
     return data, _data
 
 def runTrain(selected):
@@ -193,6 +219,7 @@ def menu():
     print("\n========== 5. 日语延长音组合\t\t\t\t==========")
     print("\n========== 6. 日语促音(停顿音)组合\t\t\t==========")
     print("\n========== 7. 日语拗音看字识音训练\t\t\t==========")
+    print("\n========== 8. 日语看字识音汇总训练\t\t\t==========")
     print("\n========== 0. 退出\t\t\t\t\t==========")
     print("\n\t\t\t\t\t\tPowered By Ruer")
 
@@ -204,7 +231,7 @@ if __name__ == "__main__":
         selected = input("Select your choice... [default is 0]: ")
         if selected == "0" or not selected:
             exit(0)
-        if int(selected) <=5 and int(selected) >=1:
+        if int(selected) <=8 and int(selected) >=1:
             runTrain(selected)
             
         else:
